3d60a44a10a43216ff6272a8a318a9dfc0802ad1
aaronbolado <aaronjohnbolado@gmail.com>
2024-09-04 09:08:04 +0800

    0: Initial setup

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    print("running main.py")
+    textnode = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(textnode)
+
+main()
